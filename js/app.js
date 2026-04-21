44dc0f4841f1df89b7df7c10b8d61c1b16facf26
montedopasto <mdopasto@gmail.com>
2026-04-21 15:49:25 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -177,6 +177,11 @@ function carregarGrafico(){
     // semanas únicas
     let labels = [...new Set(historico.map(d => d.semana))];
 
+// adicionar semanas futuras
+const previsaoLabels = ["W+1","W+2","W+3","W+4"];
+
+labels = labels.concat(previsaoLabels);
+
     const carcacaData = labels.map(semana => {
         const reg = historico.find(d => d.semana === semana && d.tipo === "carcaca");
         return reg ? reg.valor : null;
