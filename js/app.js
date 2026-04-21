e959b617ef763bd6459c849f1e9ee65adf78e8cf
montedopasto <mdopasto@gmail.com>
2026-04-21 16:51:16 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -323,3 +323,16 @@ function atualizarHeaderTempo(){
 
     document.getElementById("infoSemana").innerText = `Semana ${ano}-W${semana}`;
 }
+function getWeekNumber(date){
+
+    const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+
+    const dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+
+    const weekNum = Math.ceil((((d - yearStart) / 86400000) + 1)/7);
+
+    return String(weekNum).padStart(2, '0');
+}
