dc43a4429674031af3ded1113ef5b73cdd34a72e
montedopasto <mdopasto@gmail.com>
2026-05-13 15:37:23 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,13 @@
 let historico = [];
+async function carregarDados(){
+
+    const response = await fetch("https://script.google.com/macros/s/AKfycbwkvrOOnYAmesi1UXpUuc4D_osgPZzdYgF4WD6T6uISGj-BZUsINYv6uwfbD4tbhz5klA/exec");
+
+    const historico = await response.json();
+
+    localStorage.setItem("precos", JSON.stringify(historico));
+
+    carregarGrafico();
+    atualizarKPIs();
+    renderPrevisao();
+}
