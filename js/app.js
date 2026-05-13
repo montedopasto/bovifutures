0871ac2f894de5bbc4fcb1e44a27e9a8c0a6e4ce
montedopasto <mdopasto@gmail.com>
2026-05-13 15:37:49 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -63,33 +63,6 @@ function calcularPressaoMercado(){
     return pressao;
 }
 
-async function guardarPreco(){
-
-    const semana = document.getElementById("semana").value;
-    const tipo = document.getElementById("tipoPreco").value;
-    const valor = parseFloat(document.getElementById("valorPreco").value);
-
-    if(!semana || !valor){
-        alert("Preenche todos os campos");
-        return;
-    }
-
-    const dados = {
-        semana,
-        tipo,
-        valor
-    };
-
-    await fetch("https://script.google.com/macros/s/AKfycbwkvrOOnYAmesi1UXpUuc4D_osgPZzdYgF4WD6T6uISGj-BZUsINYv6uwfbD4tbhz5klA/exec", {
-        method: "POST",
-        body: JSON.stringify(dados)
-    });
-
-    alert("Preço guardado com sucesso!");
-
-    carregarDados();
-}
-
 function atualizarKPIs(){
 
     let historico = JSON.parse(localStorage.getItem("precos")) || [];
