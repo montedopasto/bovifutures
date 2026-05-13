a40b21208ed72268ec898f70946d7d0c6c26b527
montedopasto <mdopasto@gmail.com>
2026-05-13 15:38:02 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -11,3 +11,29 @@ async function carregarDados(){
     atualizarKPIs();
     renderPrevisao();
 }
+async function guardarPreco(){
+
+    const semana = document.getElementById("semana").value;
+    const tipo = document.getElementById("tipoPreco").value;
+    const valor = parseFloat(document.getElementById("valorPreco").value);
+
+    if(!semana || !valor){
+        alert("Preenche todos os campos");
+        return;
+    }
+
+    const dados = {
+        semana,
+        tipo,
+        valor
+    };
+
+    await fetch("https://script.google.com/macros/s/AKfycbwkvrOOnYAmesi1UXpUuc4D_osgPZzdYgF4WD6T6uISGj-BZUsINYv6uwfbD4tbhz5klA/exec", {
+        method: "POST",
+        body: JSON.stringify(dados)
+    });
+
+    alert("Preço guardado com sucesso!");
+
+    carregarDados();
+}
